c2b10d917d033cbe1df119bbea2288ef7dbd7571
Eric Skaftason <skaftasoneric@gmail.com>
2025-08-16 23:42:40 -0400

    added basic calculations and input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
 
+int numberOfIterations(int start);
+int doIteration(int value);
+
 int main() {
 
     std::cout << "******** Collatz-cpp ********\n";
 
+    std::cout << "Choose a range to calculate for." << '\n';
+
+    std::cout << "Start value:" << '\n';
+    int start_value;
+    do {
+        std::cin >> start_value;
+    } while (!(start_value > 1));
+    
+
+    std::cout << "End value:" << '\n';
+    int end_value;
+    do {
+        std::cin >> end_value;
+    } while (!(end_value > 1));
+
+    for (int i = start_value; i <= end_value; i++) {
+        int iterations = numberOfIterations(i);
+        
+        std::cout << "** Input value: " << i << " , Iterations: " << iterations << " **" << '\n';
+    }
+    
+
     return 0;
+}
+
+int doIteration(int value) {
+    if (value % 2 == 0) {
+        return value / 2;
+    } else {
+        return value * 3 + 1;
+    }
+}
+
+int numberOfIterations(int value) {
+
+    int iterations = 0;
+    while (value != 1) {
+        value = doIteration(value);
+        iterations++;
+    }
+
+    return iterations;
 }
